2508074d86f114e44c08ec4601677580b9926c29
Uttam Anand <54016758+uttam-anand@users.noreply.github.com>
2021-04-13 10:13:37 +0530

    Update running_sum_of_1d_array.cpp

--- running_sum_of_1d_array.cpp
+++ running_sum_of_1d_array.cpp
@@ -6,12 +6,4 @@ public:
             nums[i] += nums[i-1];
         return nums;
     }
-};class Solution {
-public:
-    vector<int> runningSum(vector<int>& nums) {
-        int sum =1;
-        for ( int i = 1 ; i < nums.size() ; i++ )
-            nums[i] += nums[i-1];
-        return nums;
-    }
 };
