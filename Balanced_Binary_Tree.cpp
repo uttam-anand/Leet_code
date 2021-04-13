6164830a9a8ff44dc377c873f2bf09d95eefffee
Uttam Anand <54016758+uttam-anand@users.noreply.github.com>
2021-04-13 23:37:52 +0530

    Create Balanced_Binary_Tree.cpp

---
+++ Balanced_Binary_Tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height (TreeNode * root)
+    {
+        if(root ==NULL)
+            return 0;
+        else
+            return max(height(root-> left), height(root -> right))+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL)
+            return true;
+        int lh = height(root -> left );
+        int rh = height( root -> right );
+        if ((abs(lh - rh) < 2) && isBalanced(root-> left) && isBalanced(root -> right))
+            return true;
+        return false;
+    }
+};
